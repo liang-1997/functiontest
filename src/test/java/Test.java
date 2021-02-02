42496a88d429203bdda874f8c926a16933c5da5a
liang <jie18851033137@163.com>
2021-02-02 16:20:29 +0800

    xxx

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,10 +1,13 @@
-import java.io.File;
-import java.util.Date;
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import com.sun.org.apache.xpath.internal.operations.String;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+
+import java.util.logging.Logger;
 
 public class Test {
     public static void main(String[] args) {
-
+        java.lang.String xx="123456";
+        java.lang.String s = xx.replaceAll("1", "2");
+        System.out.println(s);
     }
 }
